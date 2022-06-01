0faf02b0c8d185476ffc8973f991fbc560a224c2
xiaohuaxian <1173440402@qq.com>
2022-06-02 00:57:02 +0800

    新增了第一个聊天版本

---
+++ js/common.js
@@ -0,0 +1,11 @@
+//配置选择器
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function $$(selector) {
+    return document.querySelectorAll(selector);
+}
+
+//配置需要用到的前置域名
+const BASE_URL = 'https://study.duyiedu.com';
